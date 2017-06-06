883d9b5c84a4f8613ce08407f81d0731c1dd0994
Moon Antonio <pinchodev91@gmail.com>
2017-06-06 02:33:16 +0200

    Add UIEffectListPool.cs

--- UGUI Effects Project/Assets/UGUI Effects Proyecto/Scripts/UIEffectListPool.cs
+++ UGUI Effects Project/Assets/UGUI Effects Proyecto/Scripts/UIEffectListPool.cs
@@ -1,18 +1,23 @@
 ﻿//                                  ┌∩┐(◣_◢)┌∩┐
 //																				\\
-// UIEffectListPool.cs (00/00/0000)													\\
+// UIEffectListPool.cs (06/06/2017)												\\
 // Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
-// Descripcion:																	\\
-// Fecha Mod:		00/00/0000													\\
-// Ultima Mod:																	\\
+// Descripcion:		Pool de la lista de los efectos UI							\\
+// Fecha Mod:		06/06/2017													\\
+// Ultima Mod:		Version Inicial												\\
 //******************************************************************************\\
 
 #region Librerias
 using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
 #endregion
 
-namespace MoonAntonio
+namespace MoonAntonio.UIEffect
 {
+	/// <summary>
+	/// <para>Pool de la lista de los efectos UI.</para>
+	/// </summary>
 	public class UIEffectListPool : MonoBehaviour 
 	{
 
