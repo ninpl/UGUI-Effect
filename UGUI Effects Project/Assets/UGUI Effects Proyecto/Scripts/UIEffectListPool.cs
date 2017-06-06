cad28d5fa700cf38aa7473f67443bb7ac4a2faab
Moon Antonio <pinchodev91@gmail.com>
2017-06-06 02:28:52 +0200

    Update

---
+++ UGUI Effects Project/Assets/UGUI Effects Proyecto/Scripts/UIEffectListPool.cs
@@ -0,0 +1,20 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// UIEffectListPool.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	public class UIEffectListPool : MonoBehaviour 
+	{
+
+	}
+}
