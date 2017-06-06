d17c171147dff7a23a0c4d7bb3998fa43f9ec166
Moon Antonio <pinchodev91@gmail.com>
2017-06-06 04:31:56 +0200

    Add GradientColor.cs

---
+++ UGUI Effects Project/Assets/UGUI Effects Proyecto/Scripts/Efectos/GradientColor.cs
@@ -0,0 +1,36 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// GradientColor.cs (06/06/2017)												\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Efecto Gradient Color para la UI							\\
+// Fecha Mod:		06/06/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+#endregion
+
+/*	NOTAS	->
+	Degradado del color
+*/
+
+namespace MoonAntonio.UIEffect
+{
+	/// <summary>
+	/// <para>Efecto Gradient Color para la UI</para>
+	/// </summary>
+	[AddComponentMenu("UI/Effects/Gradient Color"), RequireComponent(typeof(Graphic))]
+	public class GradientColor : BaseMeshEffect
+	{
+		#region Metodos
+		public override void ModifyMesh(VertexHelper vh)
+		{
+
+		}
+		#endregion
+
+	}
+}
